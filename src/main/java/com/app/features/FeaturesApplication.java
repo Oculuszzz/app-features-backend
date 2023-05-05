4ad12d8c32dab81ef131314893abe60c4a2a325f
Muhammad Shahir Mokhtar <91410081+Oculuszzz@users.noreply.github.com>
2023-05-05 17:46:24 +0800

    First commit on base project

---
+++ src/main/java/com/app/features/FeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.app.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturesApplication.class, args);
+	}
+
+}
